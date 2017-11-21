9b16dadeff91080ed64f6350163924ef9dcf5211
Tatsuya Yamamoto <yamamoto.tatsuya@fujixerox.co.jp>
2017-11-21 21:36:46 +0900

    fix, declare error on ts.

---
+++ types/serialport/index.d.ts
@@ -0,0 +1,7 @@
+/**
+ * @fileOverview Temporary module declaration for serialport.
+ *
+ * TODO: replace @types/serialport after declaring valid types.
+ * @see https://github.com/DefinitelyTyped/DefinitelyTyped/tree/master/types/serialport
+ */
+declare module "serialport";
